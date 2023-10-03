fbb1d8c034e037888fa5689e0f13f3c1024d8623
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-10-03 12:11:49 -0300

    Contacts interface

---
+++ src/app/interfaces/contact.ts
@@ -0,0 +1,10 @@
+export interface Contact {
+  id: number,
+  name: string,
+  surname: string,
+  address: string,
+  email: string,
+  profilePicture: string,
+  phoneNumbers: string[],
+  enterprise: string
+}
